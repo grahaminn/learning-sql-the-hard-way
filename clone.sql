fa51dc0388d65d93f55b4e7dd91bcf580a47fed0
Andy B <andrewb@we7.com>
2014-03-24 11:56:55 +0000

    count how many subscribers streamed last week

---
+++ clone.sql
@@ -0,0 +1,5 @@
+﻿SELECT COUNT (track_streams.user_id)
+  FROM subscriptions, track_streams
+  where subscriptions.user_id = track_streams.user_id
+  AND subscriptions.subscription_type_id=40
+  AND track_streams.date > now() - INTERVAL '1 week';
